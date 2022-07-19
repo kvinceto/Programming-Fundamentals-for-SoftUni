edfa88b661b954395d9d151050c3722cf59dd44c
Krasimir Iliev <106111929+kvinceto@users.noreply.github.com>
2022-07-19 13:31:22 +0300

    Update README.md

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359(?<separator>[ -])2\k<separator>[0-9]{3}\k<separator>[0-9]{4}\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+
+           string pattern = @"\b(?<day>[0-9]{2})(?<separator>[\./-])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[0-9]{4})";
+
+           Regex regex = new Regex(pattern);
+           MatchCollection matches = regex.Matches(input);
+           foreach (Match match in matches)
+           {
+               Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+           }
+        }
+    }
+}
